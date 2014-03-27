f651fac05611801a9bf7f6caa644b627afaeff7e
Radovan Lekanovic <lekanovic@navodar.(none)>
2014-03-26 17:05:43 -0700

    Print cards using unicode.

--- cards.py
+++ cards.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 from random import randrange
+from termcolor import colored
+#Heart - u"\u2764"
+#Diamond - u"\u2666"
+#Spade - u"\u2660"
+#Club - u"\u2663"
+
 
 class Card:
 	def __init__(self,v,c,t):
@@ -13,6 +19,7 @@ class Card:
 class Deck:
 	def __init__(self,n=1):
 		self.cards = []
+
 		for y in range(0,n):
 			for x in range(1,53):
 				value = (x%13)+1
@@ -23,7 +30,36 @@ class Deck:
 
 	def print_cards(self):
 		for a in self.cards:
-			print "value: %d color %d type %d" % (a.value,a.color,a.card_type)
+			if ( a.card_type == 0):
+				if ( a.color == 0 ): #Spade
+					print colored(" %d" % (a.value),'red'),
+					print colored(u"\u2660",'red')
+				else:
+					print colored(" %d" % (a.value),'grey'),
+					print colored(u"\u2660",'grey')
+			elif( a.card_type == 1):
+				if ( a.color == 0 ): #Diamond
+					print colored(" %d" % (a.value),'red'),
+					print colored(u"\u2666",'red')
+				else:
+					print colored(" %d" % (a.value),'grey'),
+					print colored(u"\u2666",'grey')
+			elif( a.card_type == 2):
+				if ( a.color == 0 ): #Heart
+					print colored(" %d" % (a.value),'red'),
+					print colored(u"\u2764",'red')
+				else:
+					print colored(" %d" % (a.value),'grey'),
+					print colored(u"\u2764",'grey')
+			elif( a.card_type == 3):
+				if ( a.color == 0 ): #Club
+					print colored(" %d" % (a.value),'red'),
+					print colored( u"\u2663",'red')
+				else:
+					print colored(" %d" % (a.value),'grey'),
+					print colored( u"\u2663",'grey')
+			else:
+				print "ERROR"
 
 	#sattoloCycle
 	def shuffle(self):
